08eef7e0b595f359b840bfcb348c023601830718
aiert <otxoboy64@gmail.com>
2025-03-18 12:29:56 +0100

    tengo el image of the day en una clase

--- javascript/clases/clases.js
+++ javascript/clases/clases.js
@@ -1,13 +1,20 @@
-export { Article, Event, Astronaut, Images }
+export { Article, Event, Astronaut, Image, Launches, ImageOfTheDay }
 
 class Article {
-    constructor(id, title, authors, url ,image_url) {
+    constructor(id, title, authors, url ,image_url, news_site, summary, published_at) {
         this.id = id
         this.title = title
         this.authors = authors
         this.url = url
         this.image_url = image_url
+        this.news_site = news_site
+        this.summary = summary
+        this.published_at = published_at
     }
+}
+
+class Launches {
+
 }
 
 class Event {
@@ -18,6 +25,15 @@ class Astronaut {
 
 }
 
-class Images {
+class Image{
 
+}
+
+class ImageOfTheDay{
+    constructor(date, explanation, title, url) {
+        this.date = date
+        this.explanation = explanation
+        this.title = title
+        this.url = url
+    }
 }
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-import { Article, Event, Astronaut, Images} from './clases/clases.js';
+import { Article, Event, Astronaut, Image, Launches, ImageOfTheDay} from './clases/clases.js';
 
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
@@ -12,6 +12,8 @@ los voy a meter en las clases que he importado desde ./clases/clases.js
 
 */
 
+const API_KEY = 'iHU6CXT2jGDVr9hMpRdhV1Bgn7BJiShISxKrqBGv';
+
 async function datosApiClases() {
 
     //Primero voy a hacer un fetch a articles de: https://api.spaceflightnewsapi.net/v4/articles/
@@ -23,6 +25,15 @@ async function datosApiClases() {
 
         // console.log(articles)
         createArticles(articles)
+
+        //Nasa Image of the day api endpoint
+
+        const response1 = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+        const data1 = await response1.json();
+        const image = data1;
+
+        // console.log(image)
+        createImageOfTheDay(image)
 
         
       } catch (error) {
@@ -36,18 +47,23 @@ function createArticles(articles) {
     let articlesClasses = [];
 
     for (let i = 0; i < articles.length; i++) {
-        console.log(i)
 
         let article = new Article(articles[i].id, articles[i].title, articles[i].aithors, articles[i].url, articles[i].image_url);
 
         articlesClasses.push(article);
 
-        // console.log(article)
     }
 
-    console.log(articlesClasses);
+    // console.log(articlesClasses);
     //Aqui ahora usariamos el DOM para meter el array de estas clases en el html
 
+}
+
+function createImageOfTheDay(image) {
+    
+    let imageOTD = new ImageOfTheDay(image.date, image.explanation, image.title, image.url)
+
+    console.log(imageOTD)
 }
 
 datosApiClases()
